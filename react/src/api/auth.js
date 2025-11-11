00ace1de3fb6a7eb25087bb5adc4f15b36bc3862
IbragimKhaniev <newinterny@gmail.com>
2025-11-11 12:38:22 +0300

    feat(frontend): add named async `login` function to react/src/api/auth.js to fix import error and enable login request

--- react/src/api/auth.js
+++ react/src/api/auth.js
@@ -5,3 +5,9 @@ export async function getProfile() {
   const response = await instance.get('/api/auth/profile/');
   return response.data;
 }
+
+// POST /api/auth/login/ according to openapi.yml
+export async function login({ username, password }) {
+  const res = await instance.post('/api/auth/login/', { username, password });
+  return res.data;
+}
