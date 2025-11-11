cf56d1be5945df358ab39312e05c7a5d5ac466ef
IbragimKhaniev <newinterny@gmail.com>
2025-11-11 12:27:03 +0300

    chore(frontend): init Ant Design and routing; set RU locale and meta; add window.handleRoutes

--- react/src/index.js
+++ react/src/index.js
@@ -4,14 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import ru_RU from 'antd/locale/ru_RU';
+import 'antd/dist/reset.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ConfigProvider locale={ru_RU}>
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+if (typeof window !== 'undefined' && typeof window.handleRoutes === 'function') {
+  try {
+    window.handleRoutes(['/', '/login', '/register', '/profile', '/404']);
+  } catch (e) {
+    // no-op
+  }
+}
+
 reportWebVitals();
